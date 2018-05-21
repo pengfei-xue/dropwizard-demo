68729ae125007baf6588a5a82321c2e02df0407a
Pengfei Xue <pengphy@gmail.com>
2018-05-21 18:34:40 +0800

    init commit

---
+++ src/main/java/co/gmei/demoApplication.java
@@ -0,0 +1,34 @@
+package co.gmei;
+
+import io.dropwizard.Application;
+import io.dropwizard.setup.Bootstrap;
+import io.dropwizard.setup.Environment;
+
+public class demoApplication extends Application<demoConfiguration> {
+
+    public static void main(final String[] args) throws Exception {
+        new demoApplication().run(args);
+    }
+
+    @Override
+    public String getName() {
+        return "demo";
+    }
+
+    @Override
+    public void initialize(final Bootstrap<demoConfiguration> bootstrap) {
+        // TODO: application initialization
+    }
+
+    @Override
+    public void run(final demoConfiguration configuration,
+                    final Environment environment) {
+        // TODO: implement application
+        final HelloWorldResource resource = new HelloWorldResource(
+            configuration.getTemplate(),
+            configuration.getDefaultName()
+        );
+        environment.jersey().register(resource);
+    }
+
+}
